695a8ab3fac3997a7f14354ccd363cdbd92241dd
mathers08 <black.mathers08@mail.ru>
2023-04-05 22:12:38 +0300

    add local storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import users from './users/slice';
+import { loadState, saveState } from "../utils";
 
 export const setupStore = () => configureStore({
   reducer: {
     users
-  }
+  },
+  preloadedState: loadState()
 });
 
 export const store = setupStore();
+
+store.subscribe(() => saveState(store.getState()));
 export type RootState = ReturnType<typeof store.getState>
